ff766be87036a2cc95539aec790528781fbeabb8
mrheio <drg.ov00@gmail.com>
2024-08-04 00:13:36 +0300

    fix: Remove unused page, Protect dashboard

--- src/middlewares/withAuthMiddleware.ts
+++ src/middlewares/withAuthMiddleware.ts
@@ -3,7 +3,7 @@ import { NextFetchEvent, NextRequest, NextResponse } from 'next/server';
 import { isTokenExpired } from 'pocketbase';
 import { CustomMiddleware } from './chain';
 
-const PRIVATE_ROUTES = ['/private'];
+const PRIVATE_ROUTES = ['/dashboard'];
 
 const isPrivateRoute = (route: string) =>
 	PRIVATE_ROUTES.some((r) => route.startsWith(r));

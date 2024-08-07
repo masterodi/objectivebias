fe81eb36c044697cb77b4f2964fe1036ff4d9da5
mrheio <drg.ov00@gmail.com>
2024-08-07 20:54:58 +0300

    refactor: Move dashboard drawer to navbar

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,31 +1,5 @@
 type DashboardLayoutProps = Readonly<{ children: React.ReactNode }>;
 
 export default function DashboardLayout({ children }: DashboardLayoutProps) {
-	return (
-		<div>
-			{children}
-			<div className="drawer drawer-end">
-				<input
-					id="my-drawer-4"
-					type="checkbox"
-					className="drawer-toggle"
-				/>
-				<div className="drawer-side">
-					<label
-						htmlFor="my-drawer-4"
-						aria-label="close sidebar"
-						className="drawer-overlay"
-					></label>
-					<ul className="menu min-h-full w-80 bg-base-200 p-4 text-base-content">
-						<li>
-							<a>Sidebar Item 1</a>
-						</li>
-						<li>
-							<a>Sidebar Item 2</a>
-						</li>
-					</ul>
-				</div>
-			</div>
-		</div>
-	);
+	return <div>{children}</div>;
 }
--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -2,11 +2,9 @@ import { getAuth } from '@/utils';
 import Link from 'next/link';
 import { logout } from './actions';
 
-export default function Navbar() {
-	const auth = getAuth();
-
+function GuestNavbar() {
 	return (
-		<nav className="navbar sticky top-0">
+		<nav className="navbar sticky top-0 z-10">
 			<div className="navbar-start">
 				<Link
 					href="/"
@@ -17,29 +15,52 @@ export default function Navbar() {
 			</div>
 
 			<div className="navbar-end gap-4">
-				{!auth && (
-					<Link href="/register" className="btn">
-						Get Started
-					</Link>
-				)}
-				{auth && (
-					<>
+				<Link href="/register" className="btn btn-ghost">
+					Get Started
+				</Link>
+			</div>
+		</nav>
+	);
+}
+
+function UserNavbar() {
+	const auth = getAuth();
+
+	return (
+		<div className="drawer drawer-end">
+			<input
+				id="dashboard-drawer"
+				type="checkbox"
+				className="drawer-toggle"
+			/>
+			<div className="drawer-content flex flex-col">
+				<nav className="navbar w-full bg-base-300">
+					<div className="navbar-start">
+						<Link
+							href="/"
+							className="btn btn-link text-2xl font-bold uppercase"
+						>
+							brq
+						</Link>
+					</div>
+					<div className="navbar-end">
 						<span>{auth.username}</span>
 						<form action={logout}>
-							<button type="submit" className="btn">
+							<button type="submit" className="btn btn-ghost">
 								Log Out
 							</button>
 						</form>
-						<div className="drawer-content">
+						<div>
 							<label
-								htmlFor="my-drawer-4"
-								className="btn btn-ghost drawer-button"
+								htmlFor="dashboard-drawer"
+								aria-label="open sidebar"
+								className="btn btn-square btn-ghost"
 							>
 								<svg
 									xmlns="http://www.w3.org/2000/svg"
 									fill="none"
 									viewBox="0 0 24 24"
-									className="inline-block h-5 w-5 stroke-current"
+									className="inline-block h-6 w-6 stroke-current"
 								>
 									<path
 										strokeLinecap="round"
@@ -50,9 +71,32 @@ export default function Navbar() {
 								</svg>
 							</label>
 						</div>
-					</>
-				)}
+					</div>
+				</nav>
 			</div>
-		</nav>
+
+			<div className="drawer-side z-20">
+				<label
+					htmlFor="dashboard-drawer"
+					aria-label="close sidebar"
+					className="drawer-overlay"
+				></label>
+				<ul className="menu min-h-full w-80 bg-base-200 p-4">
+					<li>
+						<Link href="/dashboard">Admin Dashboard</Link>
+					</li>
+					<li>
+						<Link href="/dashboard/posts/create">
+							Create a new post
+						</Link>
+					</li>
+				</ul>
+			</div>
+		</div>
 	);
+}
+
+export default function Navbar() {
+	const auth = getAuth();
+	return !auth ? <GuestNavbar /> : <UserNavbar />;
 }

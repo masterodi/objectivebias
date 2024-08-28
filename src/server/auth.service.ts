b76d8149ac8c27fdf25931f4f1715b25c28d1083
mrheio <drg.ov00@gmail.com>
2024-08-28 17:34:37 +0300

    refactor: Create services

---
+++ src/server/auth.service.ts
@@ -0,0 +1,64 @@
+import { AuthError } from '@/errors';
+import pb from '@/pocketbase';
+import { LoginPayloadSchema, RegisterPayloadSchema, validate } from '@/schemas';
+import { LoginPayload, RegisterPayload, Session } from '@/types';
+import { AUTH_COOKIE, AuthCookieOptions } from '@/utils';
+import { cookies } from 'next/headers';
+
+async function login(payload: LoginPayload) {
+	try {
+		const data = await validate(payload, LoginPayloadSchema);
+
+		const { token, record: model } = await pb
+			.collection('users')
+			.authWithPassword(data.username, data.password);
+
+		const cookie = JSON.stringify({ token, model });
+		cookies().set(AUTH_COOKIE, cookie, AuthCookieOptions);
+	} catch (error) {
+		throw new AuthError().fromUnknown(error);
+	}
+}
+
+async function register(payload: RegisterPayload) {
+	try {
+		const data = await validate(payload, RegisterPayloadSchema);
+
+		const record = await pb.collection('users').create(data);
+
+		const { token, record: model } = await pb
+			.collection('users')
+			.authWithPassword(data.username, data.password);
+
+		const cookie = JSON.stringify({ token, model });
+		cookies().set(AUTH_COOKIE, cookie, AuthCookieOptions);
+	} catch (error) {
+		throw new AuthError().fromUnknown(error);
+	}
+}
+
+async function logout() {
+	cookies().delete(AUTH_COOKIE);
+}
+
+async function session() {
+	const authCookie = cookies().get(AUTH_COOKIE);
+	const model = authCookie?.value ? JSON.parse(authCookie.value).model : null;
+	return model as Session;
+}
+
+async function token() {
+	const authCookie = cookies().get(AUTH_COOKIE);
+	const token = authCookie?.value ? JSON.parse(authCookie.value).token : null;
+	return token as string;
+}
+
+const AuthService = {
+	login,
+	register,
+	logout,
+	session,
+	token,
+};
+
+export default AuthService;
---
+++ src/server/posts.service.ts
@@ -0,0 +1,51 @@
+import { PostsError } from '@/errors';
+import pb from '@/pocketbase';
+import { CreatePostPayloadSchema, safeValidate } from '@/schemas';
+import { CreatePostPayload, Post } from '@/types';
+import { RecordFullListOptions } from 'pocketbase';
+
+async function getList(options?: any) {
+	const queryOptions: RecordFullListOptions = {
+		sort: '-created',
+		expand: options?.with?.join(','),
+	};
+
+	const data = await pb.collection('posts').getFullList(queryOptions);
+
+	return data;
+}
+
+async function getOne(id: string, options?: any) {
+	const queryOptions: RecordFullListOptions = {
+		sort: '-created',
+		expand: options?.with?.join(','),
+	};
+
+	const data = await pb.collection<Post>('posts').getOne(id, queryOptions);
+
+	return data;
+}
+
+async function create(payload: CreatePostPayload) {
+	try {
+		const [data, validationError] = await safeValidate(
+			payload,
+			CreatePostPayloadSchema
+		);
+
+		if (validationError) throw validationError;
+
+		const res = await pb.collection('posts').create(data);
+		return { data: res };
+	} catch (error) {
+		throw new PostsError().fromUnknown(error);
+	}
+}
+
+const PostsService = {
+	getList,
+	getOne,
+	create,
+};
+
+export default PostsService;

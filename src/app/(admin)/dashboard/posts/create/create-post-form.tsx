71d29cf17adeb0b4020cabbaf2132165974809f0
masterodi <ovddrg@gmail.com>
2024-09-03 09:20:50 +0300

    feat: Add create post feature

--- src/app/(admin)/dashboard/posts/create/create-post-form.tsx
+++ src/app/(admin)/dashboard/posts/create/create-post-form.tsx
@@ -1,13 +1,74 @@
 'use client';
 
+import { createPost } from '@/app/actions/posts.actions';
+import MultiAutocompleteInput from '@/components/autocomplete-input/multi-autocomplete-input';
+import { ACOption } from '@/components/autocomplete-input/types';
+import Input from '@/components/input';
+import Editor from '@/components/rich-text/editor';
+import { useToast } from '@/components/toast';
+import { Value } from '@udecode/plate';
+import React, { useEffect, useState } from 'react';
+
 type CreatePostFormProps = {
 	options: any;
 };
 
 export default function CreatePostForm({ options }: CreatePostFormProps) {
+	const [title, setTitle] = useState('');
+	const [body, setBody] = useState([] as Value);
+	const [tags, setTags] = useState([] as ACOption<string>[]);
+	const [validation, setValidation] = useState<{
+		message: string;
+		data: Partial<
+			Record<'title' | 'body' | 'tags' | 'slug' | 'created_by', string[]>
+		>;
+	}>();
+	const [error, setError] = useState<string>();
+	const toast = useToast();
+
+	const handleCreatePost = async (e: React.FormEvent) => {
+		e.preventDefault();
+		setValidation(undefined);
+		setError(undefined);
+		const res = await createPost({
+			title,
+			body: JSON.stringify(body),
+			tags: tags.map((tag) => tag.value),
+		});
+		if (res?.validation) setValidation(res?.validation);
+		if (res?.error) setError(res?.error);
+	};
+
+	useEffect(() => {
+		if (error) toast.error(error);
+	}, [error]);
+
 	return (
-		<form className="mx-auto grid w-full max-w-xl gap-8 rounded-md bg-base-200 p-8 shadow-lg">
+		<form
+			onSubmit={handleCreatePost}
+			className="mx-auto grid w-full max-w-xl gap-8 rounded-md bg-base-200 p-8 shadow-lg"
+		>
 			<h1 className="text-3xl font-bold">Create a new post</h1>
+			<Input
+				label="Title"
+				id="title"
+				name="title"
+				value={title}
+				onChange={(e) => setTitle(e.target.value)}
+				error={validation?.data.title}
+			/>
+			<Editor
+				label="Body"
+				onChange={(newValue) => setBody(newValue)}
+				error={validation?.data.body}
+			/>
+			<MultiAutocompleteInput
+				label="Tags"
+				options={options}
+				value={tags}
+				onChange={(newValue) => setTags(newValue)}
+				error={validation?.data.tags}
+			/>
 			<button type="submit" className="btn btn-primary">
 				Publish
 			</button>
--- src/app/actions/posts.actions.ts
+++ src/app/actions/posts.actions.ts
@@ -3,11 +3,21 @@
 import { PocketbaseError, ValidationError } from '@/errors';
 import PostsService from '@/server/posts.service';
 import { CreatePostPayload } from '@/types';
+import { createSlug } from '@/utils';
+import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+import { getSession } from '../queries/auth.queries';
 
-export async function createPost(payload: CreatePostPayload) {
+type Payload = { title: string; body: string; tags: string[] };
+
+export async function createPost(payload: Payload) {
 	try {
-		await PostsService.create(payload);
+		await PostsService.create({
+			...payload,
+			slug: createSlug(payload.title),
+			created_by: (await getSession()).id,
+		});
+		revalidatePath('/dashboard');
 		redirect('/dashboard');
 	} catch (error) {
 		if (ValidationError.is<CreatePostPayload>(error)) {
@@ -18,4 +28,9 @@ export async function createPost(payload: CreatePostPayload) {
 
 		throw error;
 	}
+}
+
+export async function deletePost(id: string) {
+	const res = await PostsService.delete(id);
+	revalidatePath('/dashboard');
 }
